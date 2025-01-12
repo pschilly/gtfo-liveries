5f23969224b9f30a6e593c9fbbc048346c8063f3
Khaos 1-1 <162496244+khaos-one-one@users.noreply.github.com>
2025-01-12 17:58:11 -0500

    Add files via upload

---
+++ Liveries/HERCULES/6th AW Silent Knights/description.lua
@@ -0,0 +1,32 @@
+livery = {
+	
+	{"Herc_Map1_diff",			DIFFUSE						,	"herc_map1_diff", false};
+	{"Herc_Map1_diff_RoughMet",	ROUGHNESS_METALLIC			,	"Herc_Map1_diff_RoughMet", true};
+	{"Herc_Map1_spec",			SPECULAR					,	"Herc_Map1_spec", true};
+	
+	{"Herc_Map2_diff",			DIFFUSE						,	"herc_map2_diff", false};
+	{"Herc_Map2_diff_RoughMet",	ROUGHNESS_METALLIC			,	"Herc_Map2_diff_RoughMet", true};
+	{"Herc_Map2_spec",			SPECULAR					,	"Herc_Map2_spec", true};
+	
+	{"Herc_Map3_diff",			DIFFUSE						,	"herc_map3_diff", true};
+	{"Herc_Map3_diff_RoughMet",	ROUGHNESS_METALLIC			,	"Herc_Map3_diff_RoughMet", true};
+	{"Herc_Map3_spec",			SPECULAR					,	"Herc_Map3_spec", true};
+	
+	{"Herc_Map4_diff",			DIFFUSE						,	"herc_map4_diff", true};
+	{"Herc_Map4_diff_RoughMet",	ROUGHNESS_METALLIC			,	"Herc_Map4_diff_RoughMet", true};
+	{"Herc_Map4_spec",			SPECULAR					,	"Herc_Map4_spec", true};
+	
+	{"Herc_Map5_diff",			DIFFUSE						,	"herc_map5_diff", false};
+	{"Herc_Map5_diff_RoughMet",	ROUGHNESS_METALLIC			,	"Herc_Map5_diff_RoughMet", true};
+	{"Herc_Map5_spec",			SPECULAR					,	"Herc_Map5_spec", true};
+	
+	{"Herc_Pilots_diff",		DIFFUSE						,	"Herc_Pilots_diff", false};
+	{"Herc_Pilots_normal",		NORMAL						,	"Herc_Pilots_normal", false};
+	{"Herc_Pilots_spec",		SPECULAR					,	"Herc_Pilots_spec", false};
+	
+	{"Massive_Ordance_Air_Blast_GBU-43B",	DIFFUSE,		"Massive_Ordance_Air_Blast_GBU-43B", false};
+}
+name = "6TH AW Silent Knights"
+
+countries = "USA"
+
