da441fdd45da83607bd8b3f0464e0e9b87f86755
Schilly <patrick.schillemat@gmail.com>
2025-03-13 18:10:04 -0400

    Version 1.0

---
+++ Liveries/A-10CII/191st Grey/description.lua
@@ -0,0 +1,20 @@
+livery = {
+	{"A-10C_PAINT_1-a", 0 ,"A-10_1-a-191st"	,false};
+	{"A-10C_PAINT_1-b", 0 ,"A-10_1-b-191st"	,false};
+	{"A-10C_PAINT_1-c", 0 ,"A-10_1-c-191st"	,false};
+	{"A-10C_PAINT_1-d", 0 ,"A-10_1-d-191st"	,false};
+	{"A-10C_PAINT_1-e", 0 ,"A-10_1-e-191st"	,false};
+	{"A-10C_PAINT_1-f", 0 ,"A-10_1-f-191st"	,false};
+	{"A-10C_PAINT_1-g", 0 ,"A-10_1-g-191st"	,false};
+	{"A-10C_PAINT_1-h", 0 ,"A-10_1-h-191st"	,false};
+	{"A-10C_PAINT_1-i", 0 ,"A-10_1-i-191st"	,false};
+	{"A-10C_PAINT_1-j", 0 ,"A-10_1-j-191st"	,false};
+	{"A-10C_PAINT_1-k", 0 ,"A-10_1-k-191st"	,false};
+	{"A-10C_PAINT_1-L", 0 ,"A-10_1-l-191st"	,false};
+	{"A-10_Number", 0 ,"numbers",false};
+	{"A-10_Number2", 0 ,"empty",true};
+	{"A-10_Number_Noze_F", 0 ,"empty",true};
+	{"A-10_Number_Noze_T", 0 ,"numbers",false};
+	{"A-10_Number_Wheel", 0 ,"empty",true};
+}
+countries = {"USA"}
---
+++ README.md
@@ -0,0 +1,5 @@
+<p align="center"><img src="https://globaltaskforceoverlord.org/storage/images/Logo.png" alt="Global Task Force Overlord" width="512"/></p>
+
+# Global Task Force Overlord Official Livery Pack
+
+The follow repository was created for the purpose of providing a method for members of Global Task Force Overlord (GTFO) to both collaborate on the creation of, as well as, distribution of custom liveries for usage within our Digital Combat Simulator (DCS) community.
