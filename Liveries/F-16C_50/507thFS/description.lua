ac33d10de8e11c00815e64389bff28fabc0e7109
Schilly <patrick.schillemat@gmail.com>
2025-05-03 15:25:26 -0400

    F-16C Line Bird

---
+++ Liveries/F-16C_50/507thFS/description.lua
@@ -0,0 +1,168 @@
+livery = {
+
+-- F16_bl50_Main_1
+	{"F16_bl50_Main_1",	0,	                "f16_bl50_Main_1",			false};
+	{"F16_bl50_Main_1",	1,	                "f16_bl50_Main_1_normal",	true};
+	{"F16_bl50_Main_1",	ROUGHNESS_METALLIC,	"F16_bl50_Main_1_RoughMet", false};
+	
+
+-- F16_bl50_Main_2
+	{"F16_bl50_Main_2",	0,	                "f16_bl50_Main_2",			false};
+	{"F16_bl50_Main_2",	1,	                "f16_bl50_Main_2_normal",	true};
+	{"F16_bl50_Main_2",	ROUGHNESS_METALLIC,	"F16_bl50_Main_2_RoughMet",	false};
+	
+
+
+-- F16_bl50_Main_3
+	{"F16_bl50_Main_3",	0,	                "f16_bl50_Main_3",			false};
+	{"F16_bl50_Main_3",	1,	                "f16_bl50_Main_3_normal",	true};
+	{"F16_bl50_Main_3",	ROUGHNESS_METALLIC,	"F16_bl50_Main_3_RoughMet",	false};
+	
+
+
+-- F16_bl50_Kil
+	{"F16_bl50_Kil",	0,	                "f16_bl50_Kil",				false};
+	{"F16_bl50_Kil",	1,	                "f16_bl50_kil_normal",		true};
+	{"F16_bl50_Kil",	ROUGHNESS_METALLIC,	"F16_bl50_Kil_RoughMet",	false};
+
+
+
+-- F16_bl50_wing_L
+	{"F16_bl50_wing_L",	0,	                "f16_bl50_wing_L",			false};
+	{"F16_bl50_wing_L",	1,	                "f16_bl50_wing_L_Normal",	true};
+	{"F16_bl50_wing_L",	ROUGHNESS_METALLIC,	"F16_bl50_wing_L_RoughMet",	false};
+	
+
+
+-- F16_bl50_wing_R
+	{"F16_bl50_wing_R",	0,	                "f16_bl50_wing_R",			false};
+	{"F16_bl50_wing_R",	1,	                "f16_bl50_wing_R_Normal",	true};
+	{"F16_bl50_wing_R",	ROUGHNESS_METALLIC,	"F16_bl50_wing_R_RoughMet",	false};
+	
+
+
+-- F16_bl50_Engine
+	{"F16_bl50_Engine",	0,	                "f16_bl50_Engine",			false};
+	{"F16_bl50_Engine",	1,	                "f16_bl50_engine_normal",	true};
+	{"F16_bl50_Engine",	ROUGHNESS_METALLIC,	"f16_bl50_Engine_roughmet",	false};
+
+
+
+-- F16_bl50_Wing_Pylon_1
+	{"F16_bl50_Wing_Pylon_1", 0,	                "f16_bl50_wing_pylon_1",			true};
+	{"F16_bl50_Wing_Pylon_1", 1,	                "f16_bl50_wing_pylon_1_normal",		true};
+	{"F16_bl50_Wing_Pylon_1", ROUGHNESS_METALLIC,	"f16_bl50_wing_pylon_1_roughmet",	true};
+
+
+
+-- F16_bl50_Wing_Pylon_2
+	{"F16_bl50_Wing_Pylon_2", 0,	                "f16_bl50_wing_pylon_2",		    true};
+	{"F16_bl50_Wing_Pylon_2", 1,	                "f16_bl50_wing_pylon_2_normal",		true};
+	{"F16_bl50_Wing_Pylon_2", ROUGHNESS_METALLIC,	"f16_bl50_wing_pylon_2_roughmet",	true};
+
+
+
+-- F16_bl50_LAU_129
+	{"LAU_129", 0,	                                "LAU_129_diff",		                true};
+	{"LAU_129", 1,	                                "LAU_129_NM",		                true};
+	{"LAU_129", ROUGHNESS_METALLIC,	                "LAU_129_diff_RoughMet",	        true}; 
+
+
+
+-- F16_bl50_Fuel_Tank_300Gal
+	{"Fuel_Tank_300Gal", 0,	                        "Fuel_Tank_300Gal",		            false};
+	{"Fuel_Tank_300Gal", 1,	                        "Fuel_Tank_300Gal_Normal",		    true};
+	{"Fuel_Tank_300Gal", ROUGHNESS_METALLIC,	    "Fuel_Tank_300Gal_RoughMet",	    false};
+	
+
+
+-- F16_bl50_Fuel_Tank_370Gal
+	{"F_16_Tank_370", 0,	                        "Fuel_Tank_370gal_diff",		        false};
+	{"F_16_Tank_370", 1,	                        "Fuel_Tank_370gal_NM",		            true};
+	{"F_16_Tank_370", ROUGHNESS_METALLIC,	        "Fuel_Tank_370gal_diff_RoughMet",	    false};
+
+
+
+-- Pilot
+		
+	{"pilot_F16_patch",	 0,	                    "pilot_f16_patch",				false};
+	{"pilot_F16_patch",	 1,	                    "pilot_f16_patch_nm",			false};
+	{"pilot_F16_patch",	 ROUGHNESS_METALLIC,	"pilot_f16_patch_roughmet",		true};
+
+
+
+	{"pilot_F16_helmet_HMD",	DIFFUSE			,	"pilot_f16_helmet_hmd", false};
+	{"pilot_F16_helmet_HMD",	NORMAL_MAP			,	"pilot_f16_helmet_hmd_normal", true};
+	{"pilot_F16_helmet_HMD",	SPECULAR			,	"pilot_f16_helmet_hmd_roughmet", true};
+	
+	{"pilot_F16_helmet_gray",	DIFFUSE			,	"pilot_f16_helmet_gray", false};
+	{"pilot_F16_helmet_gray",	NORMAL_MAP			,	"pilot_f16_helmet_gray_normal", true};
+	{"pilot_F16_helmet_gray",	SPECULAR			,	"pilot_f16_helmet_gray_roughmet", true};
+
+
+-- BORT NUMBER--------------------------------------------------------------------------------------------------------------
+
+
+
+--F16_bl50_FIN_DECAL	
+    {"F16_bl50_FIN_DECAL",	0,	                "f16_bl50_kil",				false};
+	{"F16_bl50_FIN_DECAL",	1,	                "f16_bl50_kil_normal",		true};
+	{"F16_bl50_FIN_DECAL",	ROUGHNESS_METALLIC,	"f16_bl50_kil_roughmet",	false};
+	{"F16_bl50_FIN_DECAL",	DECAL,	            "F16_AF_91_Decal",	false};
+
+--F16_bl50_FIN_BORT_NUMBER
+    {"F16_bl50_FIN_BORT_NUMBER",	0,	                "f16_bl50_kil",				false};
+	{"F16_bl50_FIN_BORT_NUMBER",	1,	                "f16_bl50_kil_normal",		true};
+	{"F16_bl50_FIN_BORT_NUMBER",	ROUGHNESS_METALLIC,	"f16_bl50_kil_roughmet",	false};
+	{"F16_bl50_FIN_BORT_NUMBER",	DECAL,	            "F16_bort_number",	false};
+
+
+
+
+
+
+--F16_bl50_NOUSE_DECAL
+    {"F16_bl50_MAT1_BORT_NUMBER_X100",	0,	                "F16_bl50_Main_1",			false};
+	{"F16_bl50_MAT1_BORT_NUMBER_X100",	1,	                "f16_bl50_main_1_normal",	true};
+	{"F16_bl50_MAT1_BORT_NUMBER_X100",	ROUGHNESS_METALLIC,	"F16_bl50_Main_1_RoughMet", false};
+	{"F16_bl50_MAT1_BORT_NUMBER_X100",	DECAL,	            "F16_bort_number", false};
+
+--F16_bl50_REFUEL_DECAL 
+    {"F16_bl50_MAT1_BORT_NUMBER_DECAL",	0,	                "F16_bl50_Main_1",			false};
+	{"F16_bl50_MAT1_BORT_NUMBER_DECAL",	1,	                "f16_bl50_main_1_normal",	true};
+	{"F16_bl50_MAT1_BORT_NUMBER_DECAL",	ROUGHNESS_METALLIC,	"F16_bl50_Main_1_RoughMet", false};
+	{"F16_bl50_MAT1_BORT_NUMBER_DECAL",	DECAL,	            "F16_AF_91_Decal", false};
+
+--F16_bl50_NOUSE AND REFUEL BORT_NUMBER
+    {"F16_bl50_MAT1_BORT_NUMBER",	0,	                "F16_bl50_Main_1",			false};
+	{"F16_bl50_MAT1_BORT_NUMBER",	1,	                "f16_bl50_main_1_normal",	true};
+	{"F16_bl50_MAT1_BORT_NUMBER",	ROUGHNESS_METALLIC,	"F16_bl50_Main_1_RoughMet", false};
+	{"F16_bl50_MAT1_BORT_NUMBER",	DECAL,	            "F16_bort_number", false};
+
+
+
+
+
+--F16_bl50_INTAKE_BORT_NUMBER	
+    {"F16_bl50_INTAKE_BORT_NUMBER",	0,	                "F16_bl50_Main_3",				false};
+	{"F16_bl50_INTAKE_BORT_NUMBER",	1,	                "f16_bl50_main_3_normal",		true};
+	{"F16_bl50_INTAKE_BORT_NUMBER",	ROUGHNESS_METALLIC,	"f16_bl50_Main_3_RoughMet",	false};
+	{"F16_bl50_INTAKE_BORT_NUMBER",	DECAL,	            "F16_bort_number",	false};
+
+
+}
+
+
+name = "GTFO - 507th Fighter Squadron"
+order     = 1
+
+--BORT NUMBER TYPE ANIMATION------------------------------------------------------------------------------------------------------
+
+custom_args = 
+{
+ 
+[1000] = 0.0, -- change of type of board number (0.0 -default USA, 0.1- )
+[1001] = 1.0, -- vis refuel board number 
+[1002] = 1.0, -- change of type intake board number 
+[1003] = 0.0, -- vis nouse board number 
+}
