f014eadee39adc5396985f39cc6d9da115eb31f5
Schilly <patrick.schillemat@gmail.com>
2025-12-29 12:49:05 -0500

    Changed the F-14A (Early) to reference the non early variation to save disk space.

--- Liveries/F-14A-135-GR-Early/VF-142 Ghostriders AE200 (1976)/description.lua
+++ Liveries/F-14A-135-GR-Early/VF-142 Ghostriders AE200 (1976)/description.lua
@@ -3,73 +3,67 @@ livery = {
 
 --Diffuse-------------------------------------
 
-{"HB_F14_EXT_01", 0 ,"HB_F14_EXT_01_VF-142_200",false};
-{"HB_F14_EXT_02", 0 ,"HB_F14_EXT_02_VF-142_200",false};
-{"HB_F14_EXT_03", 0 ,"HB_F14_EXT_03_VF-142_200",false};
-{"HB_F14_EXT_04", 0 ,"HB_F14_EXT_04_VF-142_200",false};
-{"HB_F14_EXT_TAIL", 0 ,"HB_F14_EXT_TAIL_VF-142_200",false};
-{"HB_F14_WING_LEFT_01", 0 ,"HB_F14_WING_LEFT_VF-142_200",false};
-{"HB_F14_WING_RIGHT", 0 ,"HB_F14_WING_RIGHT_VF-142_200",false};
+{"HB_F14_EXT_01", 0 ,"../../F-14A-135-GR/VF-142 Ghostriders AE200 (1976)/HB_F14_EXT_01_VF-142_200",false};
+{"HB_F14_EXT_02", 0 ,"../../F-14A-135-GR/VF-142 Ghostriders AE200 (1976)/HB_F14_EXT_02_VF-142_200",false};
+{"HB_F14_EXT_03", 0 ,"../../F-14A-135-GR/VF-142 Ghostriders AE200 (1976)/HB_F14_EXT_03_VF-142_200",false};
+{"HB_F14_EXT_04", 0 ,"../../F-14A-135-GR/VF-142 Ghostriders AE200 (1976)/HB_F14_EXT_04_VF-142_200",false};
+{"HB_F14_EXT_TAIL", 0 ,"../../F-14A-135-GR/VF-142 Ghostriders AE200 (1976)/HB_F14_EXT_TAIL_VF-142_200",false};
+{"HB_F14_WING_LEFT_01", 0 ,"../../F-14A-135-GR/VF-142 Ghostriders AE200 (1976)/HB_F14_WING_LEFT_VF-142_200",false};
+{"HB_F14_WING_RIGHT", 0 ,"../../F-14A-135-GR/VF-142 Ghostriders AE200 (1976)/HB_F14_WING_RIGHT_VF-142_200",false};
 
-{"HB_F14_EXT_PHOENIXRAILS", 0 ,"HB_F14_EXT_PHOENIXPYLONS_Factory",false};
-{"HB_F14_EXT_DROPTANKS", 0 ,"HB_F14_EXT_DROPTANK_Factory",false};
-{"HB_F14_EXT_PYLONS", 0 ,"HB_F14_EXT_PYLONS_01_Factory",false};
-{"HB_F14_TCS", 0 ,"HB_F14_EXT_TCS_Factory",false};
-{"HB_F14_EXT_LAU-7", 0 ,"HB_F14_EXT_LAU_7",false};
-{"HB_F14_EXT_INTAKERAMPS", 0,"HB_F14_EXT_INTAKERAMPS",false};
+{"HB_F14_EXT_PHOENIXRAILS", 0 ,"../../F-14A-135-GR/VF-142 Ghostriders AE200 (1976)/HB_F14_EXT_PHOENIXPYLONS_Factory",false};
+{"HB_F14_EXT_DROPTANKS", 0 ,"../../F-14A-135-GR/VF-142 Ghostriders AE200 (1976)/HB_F14_EXT_DROPTANK_Factory",false};
+{"HB_F14_EXT_PYLONS", 0 ,"../../F-14A-135-GR/VF-142 Ghostriders AE200 (1976)/HB_F14_EXT_PYLONS_01_Factory",false};
+{"HB_F14_TCS", 0 ,"../../F-14A-135-GR/VF-142 Ghostriders AE200 (1976)/HB_F14_EXT_TCS_Factory",false};
+{"HB_F14_EXT_LAU-7", 0 ,"../../F-14A-135-GR/VF-142 Ghostriders AE200 (1976)/HB_F14_EXT_LAU_7",false};
+{"HB_F14_EXT_INTAKERAMPS", 0,"../../F-14A-135-GR/VF-142 Ghostriders AE200 (1976)/HB_F14_EXT_INTAKERAMPS",false};
 
-{"HB_F14_EXT_PILOT_HELMET", 0 ,"HB_HGU-33_VF-142",false};
-{"HB_F14_EXT_RIO_HELMET", 0 ,"HB_HGU-33_VF-142",false};
-{"HB_F14_LOD1_3in1", 0 ,"HB_F14_LOD1_3in1_VF-142_200",false};
-{"HB_F14_EXT_PILOT", 0 ,"HB_F14_EXT_PILOT_VF-142",false};
-{"HB_F14_EXT_PILOT_SUIT", 0 ,"HB_F14_EXT_PILOT_SUIT_VF-142",false};
-
-{"AIM-7M",	0	,"aim_7f_Diff", false};
-{"HB_F14_AIM-54A", 0 ,"HB_F14_AIM-54_Early",false};
+{"HB_F14_EXT_PILOT_HELMET", 0 ,"../../F-14A-135-GR/VF-142 Ghostriders AE200 (1976)/HB_HGU-33_VF-142",false};
+{"HB_F14_EXT_RIO_HELMET", 0 ,"../../F-14A-135-GR/VF-142 Ghostriders AE200 (1976)/HB_HGU-33_VF-142",false};
+{"HB_F14_LOD1_3in1", 0 ,"../../F-14A-135-GR/VF-142 Ghostriders AE200 (1976)/HB_F14_LOD1_3in1_VF-142_200",false};
+{"HB_F14_EXT_PILOT", 0 ,"../../F-14A-135-GR/VF-142 Ghostriders AE200 (1976)/HB_F14_EXT_PILOT_VF-142",false};
+{"HB_F14_EXT_PILOT_SUIT", 0 ,"../../F-14A-135-GR/VF-142 Ghostriders AE200 (1976)/HB_F14_EXT_PILOT_SUIT_VF-142",false};
+{"AIM-7M",	0	,"../../F-14A-135-GR/VF-142 Ghostriders AE200 (1976)/aim_7f_Diff", false};
+{"HB_F14_AIM-54A", 0 ,"../../F-14A-135-GR/VF-142 Ghostriders AE200 (1976)/HB_F14_AIM-54_Early",false};
 
 --Roughmet------------------------------------
 
-{"HB_F14_EXT_01", 13 ,"HB_F14_EXT_01_RoughMet",false};
-{"HB_F14_EXT_02", 13 ,"HB_F14_EXT_02_RoughMet",false};
-{"HB_F14_EXT_03", 13 ,"HB_F14_EXT_03_RoughMet",false};
-{"HB_F14_EXT_04", 13 ,"HB_F14_EXT_04_RoughMet",false};
-{"HB_F14_EXT_TAIL", 13 ,"HB_F14_EXT_TAIL_RoughMet",false};
-{"HB_F14_WING_LEFT_01", 13 ,"HB_F14_WING_LEFT_RoughMet",false};
-{"HB_F14_WING_RIGHT", 13 ,"HB_F14_WING_RIGHT_RoughMet",false};
+{"HB_F14_EXT_01", 13 ,"../F-14A-135-GR/VF-142 Ghostriders AE200 (1976)/HB_F14_EXT_01_RoughMet",false};
+{"HB_F14_EXT_02", 13 ,"../../F-14A-135-GR/VF-142 Ghostriders AE200 (1976)/HB_F14_EXT_02_RoughMet",false};
+{"HB_F14_EXT_03", 13 ,"../../F-14A-135-GR/VF-142 Ghostriders AE200 (1976)/HB_F14_EXT_03_RoughMet",false};
+{"HB_F14_EXT_04", 13 ,"../../F-14A-135-GR/VF-142 Ghostriders AE200 (1976)/HB_F14_EXT_04_RoughMet",false};
+{"HB_F14_EXT_TAIL", 13 ,"../../F-14A-135-GR/VF-142 Ghostriders AE200 (1976)/HB_F14_EXT_TAIL_RoughMet",false};
+{"HB_F14_WING_LEFT_01", 13 ,"../../F-14A-135-GR/VF-142 Ghostriders AE200 (1976)/HB_F14_WING_LEFT_RoughMet",false};
+{"HB_F14_WING_RIGHT", 13 ,"../../F-14A-135-GR/VF-142 Ghostriders AE200 (1976)/HB_F14_WING_RIGHT_RoughMet",false};
 
-{"HB_F14_EXT_PHOENIXRAILS", 13 ,"HB_F14_EXT_PHOENIXPYLONS_RoughMet",false};
-{"HB_F14_EXT_DROPTANKS", 13 ,"HB_F14_EXT_DROPTANK_Roughmet",false};
-{"HB_F14_EXT_PYLONS", 13 ,"HB_F14_EXT_PYLONS_01_Roughmet",false};
-{"HB_F14_TCS", 13 ,"HB_F14_EXT_TCS_Roughmet",false};
-{"HB_F14_EXT_LAU-7", 13 ,"HB_F14_EXT_LAU_7_Roughmet",false};
-{"HB_F14_EXT_INTAKERAMPS", 13,"HB_F14_EXT_INTAKERAMPS_Roughmet",false};
+{"HB_F14_EXT_PHOENIXRAILS", 13 ,"../../F-14A-135-GR/VF-142 Ghostriders AE200 (1976)/HB_F14_EXT_PHOENIXPYLONS_RoughMet",false};
+{"HB_F14_EXT_DROPTANKS", 13 ,"../../F-14A-135-GR/VF-142 Ghostriders AE200 (1976)/HB_F14_EXT_DROPTANK_Roughmet",false};
+{"HB_F14_EXT_PYLONS", 13 ,"../../F-14A-135-GR/VF-142 Ghostriders AE200 (1976)/HB_F14_EXT_PYLONS_01_Roughmet",false};
+{"HB_F14_TCS", 13 ,"../../F-14A-135-GR/VF-142 Ghostriders AE200 (1976)/HB_F14_EXT_TCS_Roughmet",false};
+{"HB_F14_EXT_LAU-7", 13 ,"../../F-14A-135-GR/VF-142 Ghostriders AE200 (1976)/HB_F14_EXT_LAU_7_Roughmet",false};
+{"HB_F14_EXT_INTAKERAMPS", 13,"../../F-14A-135-GR/VF-142 Ghostriders AE200 (1976)/HB_F14_EXT_INTAKERAMPS_Roughmet",false};
 
-{"HB_F14_EXT_PILOT_HELMET", 13 ,"HB_F14_EXT_PILOT_HELMET_RoughMet",false};
-{"HB_F14_EXT_RIO_HELMET", 13 ,"HB_F14_EXT_PILOT_HELMET_RoughMet",false};
-{"HB_F14_LOD1_3in1", 13 ,"HB_F14_LOD1_3in1_Roughmet",false};
+{"HB_F14_EXT_PILOT_HELMET", 13 ,"../../F-14A-135-GR/VF-142 Ghostriders AE200 (1976)/HB_F14_EXT_PILOT_HELMET_RoughMet",false};
+{"HB_F14_EXT_RIO_HELMET", 13 ,"../../F-14A-135-GR/VF-142 Ghostriders AE200 (1976)/HB_F14_EXT_PILOT_HELMET_RoughMet",false};
+{"HB_F14_LOD1_3in1", 13 ,"../../F-14A-135-GR/VF-142 Ghostriders AE200 (1976)/HB_F14_LOD1_3in1_Roughmet",false};
+{"AIM-7M",	13	,"../../F-14A-135-GR/VF-142 Ghostriders AE200 (1976)/aim_7f_Diff_RoughMet", false};
+{"HB_F14_AIM-54A", 13 ,"../../F-14A-135-GR/VF-142 Ghostriders AE200 (1976)/HB_F14_AIM-54A_Roughmet",false};
 
-{"AIM-7M",	13	,"aim_7f_Diff_RoughMet", false};
-{"HB_F14_AIM-54A", 13 ,"HB_F14_AIM-54A_Roughmet",false};
-
-{"HB_F14_CPT_CANOPYGLASS", 13 ,"hb_f14_cpt_canopyglass_roughmet", false};
-{"HB_F14_CPT_WINDSCREEN", 13 ,"hb_f14_cpt_canopyglass_roughmet", false};
-
+{"HB_F14_CPT_CANOPYGLASS", 13 ,"../../F-14A-135-GR/VF-142 Ghostriders AE200 (1976)/hb_f14_cpt_canopyglass_roughmet", false};
+{"HB_F14_CPT_WINDSCREEN", 13 ,"../../F-14A-135-GR/VF-142 Ghostriders AE200 (1976)/hb_f14_cpt_canopyglass_roughmet", false};
 --Normal--------------------------------------
 
-{"HB_F14_EXT_02", 1 ,"HB_F14_EXT_02_Nrm",false};
-{"HB_F14_EXT_03", 1 ,"HB_F14_EXT_03_Nrm",false};
-{"HB_F14_EXT_04", 1 ,"HB_F14_EXT_04_Nrm",false};
-{"HB_F14_EXT_TAIL", 1 ,"HB_F14_EXT_TAIL_Nrm",false};
+{"HB_F14_EXT_02", 1 ,"../../F-14A-135-GR/VF-142 Ghostriders AE200 (1976)/HB_F14_EXT_02_Nrm",false};
+{"HB_F14_EXT_03", 1 ,"../../F-14A-135-GR/VF-142 Ghostriders AE200 (1976)/HB_F14_EXT_03_Nrm",false};
+{"HB_F14_EXT_04", 1 ,"../../F-14A-135-GR/VF-142 Ghostriders AE200 (1976)/HB_F14_EXT_04_Nrm",false};
+{"HB_F14_EXT_TAIL", 1 ,"../../F-14A-135-GR/VF-142 Ghostriders AE200 (1976)/HB_F14_EXT_TAIL_Nrm",false};
 
-{"HB_F14_EXT_PILOT_HELMET", 1 ,"HB_F14_EXT_PILOT_HELMET_Nrm",false};
-{"HB_F14_EXT_RIO_HELMET", 1 ,"HB_F14_EXT_PILOT_HELMET_Nrm",false};
-{"HB_F14_EXT_PILOT_SUIT", 1 ,"HB_F14_EXT_PILOT_SUIT_Nrm",false};
+{"HB_F14_EXT_PILOT_HELMET", 1 ,"../../F-14A-135-GR/VF-142 Ghostriders AE200 (1976)/HB_F14_EXT_PILOT_HELMET_Nrm",false};
+{"HB_F14_EXT_RIO_HELMET", 1 ,"../../F-14A-135-GR/VF-142 Ghostriders AE200 (1976)/HB_F14_EXT_PILOT_HELMET_Nrm",false};
+{"HB_F14_EXT_PILOT_SUIT", 1 ,"../../F-14A-135-GR/VF-142 Ghostriders AE200 (1976)/HB_F14_EXT_PILOT_SUIT_Nrm",false};
+{"AIM-7M",	1	,"../../F-14A-135-GR/VF-142 Ghostriders AE200 (1976)/aim_7f_Diff_Normal", false};
 
-{"AIM-7M",	1	,"aim_7f_Diff_Normal", false};
-
-{"HB_F14_CPT_CANOPYGLASS", 1 ,"hb_f14_cpt_canopyglass_nrm", false};
-{"HB_F14_CPT_WINDSCREEN", 1 ,"hb_f14_cpt_canopyglass_nrm", false};
+{"HB_F14_CPT_CANOPYGLASS", 1 ,"../../F-14A-135-GR/VF-142 Ghostriders AE200 (1976)/hb_f14_cpt_canopyglass_nrm", false};
+{"HB_F14_CPT_WINDSCREEN", 1 ,"../../F-14A-135-GR/VF-142 Ghostriders AE200 (1976)/hb_f14_cpt_canopyglass_nrm", false};
 }
 name = "VF-142 Ghostriders AE200 (1976)"
-
---countries = {"USA", "RUS", "FRA", "UKR", "SPN", "NETH", "TUR", "BEL", "GER", "NOR", "CAN", "DEN", "UK", "GRG", "ISR", "ABH", "RSO"}
