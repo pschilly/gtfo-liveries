faf6b97b88cec9b56930f8ab574eef27f7c7c2e5
Patrick <patrick.schillemat@gmail.com>
2026-06-19 17:57:32 -0400

    F-100D Initial Livery - Phang Rang GTFO Style

---
+++ Liveries/F-100D/Global Task Force Overlord/description.lua
@@ -0,0 +1,26 @@
+livery = 
+{
+	{"GD_F100_FUSE_1",	DIFFUSE			,	"gd_f100_ext_01", false};
+	{"GD_F100_FUSE_1",	ROUGHNESS_METALLIC	,	"gd_f100_ext_01_roughmet", false};
+
+	{"GD_F100_FUSE_2",	DIFFUSE			,	"gd_f100_ext_02", false};
+	{"GD_F100_FUSE_2",	ROUGHNESS_METALLIC	,	"gd_f100_ext_02_roughmet", false};
+
+	{"GD_F100_TAIL",	DIFFUSE			,	"gd_f100_ext_03", false};
+	{"GD_F100_TAIL",	ROUGHNESS_METALLIC	,	"gd_f100_ext_03_roughmet", false};
+
+	{"GD_F100_WING_L",	DIFFUSE			,	"gd_f100_ext_04", false};
+	{"GD_F100_WING_L",	ROUGHNESS_METALLIC	,	"gd_f100_ext_04_roughmet", false};
+
+	{"GD_F100_WING_R",	DIFFUSE			,	"gd_f100_ext_05", false};
+	{"GD_F100_WING_R",	ROUGHNESS_METALLIC	,	"gd_f100_ext_05_roughmet", false};
+
+	{"GD_F100_INTAKE",	DIFFUSE			,	"gd_f100_ext_10", false};
+	{"GD_F100_INTAKE",	ROUGHNESS_METALLIC	,	"gd_f100_ext_10_roughmet", false};
+
+	{"GD_F100_INTERIOR",	DIFFUSE			,	"gd_f100_ext_11", false};
+	{"GD_F100_INTERIOR",	ROUGHNESS_METALLIC	,	"gd_f100_ext_11_roughmet", false};
+}
+
+name = "Global Task Force Overlord"
+order     = 1
